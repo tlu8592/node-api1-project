2837af37d502cd9a0978131112aa3a17f5a866ee
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-22 22:52:57 -0400

    get endpoint written: /api/users/:id

--- api/server.js
+++ api/server.js
@@ -18,6 +18,24 @@ server.get('/api/users', (req, res) => {
         })
 })
 
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            }
+            res.json(user);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The user information could not be retrieved",
+                err: err.message
+            })
+        })
+})
+
 // if no other endpoint is found
 server.use('*', (req, res) => {
     res.status(404).json({
