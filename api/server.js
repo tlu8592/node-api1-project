7df146c9c2b9e7bd163bc5640df1d7f9de7841aa
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-22 22:42:42 -0400

    get endpoint written: /api/users

--- api/server.js
+++ api/server.js
@@ -3,6 +3,21 @@ const express = require('express');
 
 const server = express();
 
+const User = require('./users/model');
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "error getting users",
+                err: err.message
+            })
+        })
+})
+
 // if no other endpoint is found
 server.use('*', (req, res) => {
     res.status(404).json({
