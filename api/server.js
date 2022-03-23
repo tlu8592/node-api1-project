15dfaf973a5f4134b08a190ca69eccc4ac58855b
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-22 22:32:55 -0400

    completed server setup

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+// if no other endpoint is found
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
