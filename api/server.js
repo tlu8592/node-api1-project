e24d30679a19eaa05437d964ed5b443644f2698a
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-23 01:58:12 -0400

    put endpoint written: /api/users/:id

--- api/server.js
+++ api/server.js
@@ -77,8 +77,32 @@ server.delete('/api/users/:id', async (req, res) => {
     }
 })
 
-server.put('/api/users/:id', (req, res) => {
-
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id);
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user"
+                })
+            } else {
+                const updatedUser = await User.update(
+                    req.params.id, 
+                    req.body
+                );
+                res.status(200).json(updatedUser);
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "error updating user",
+            err: err.message
+        })
+    }
 })
 
 // if no other endpoint is found
